7333da5cdfb6397e81b210bd471dba2cdcb2e7df
George <developer@monternos.net>
2019-12-04 23:03:55 +0100

    [added] neccessary translations

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -4,6 +4,17 @@ const en = {
 		home: 'Home',
 		forum: 'Forum',
 		login: 'Login'
+	},
+	monitoring: {
+		activeChatreports: 'Active Chatreports',
+		reportedPlayer: 'Reported Player',
+		reportedFrom: 'Reported From',
+		lastMessage: 'Last Message',
+		status: 'Status',
+		available: 'Available',
+		unavailable: 'Unavailable',
+		inProgress: 'In Progress',
+		takeOver: 'Take Over'
 	}
 };
 
